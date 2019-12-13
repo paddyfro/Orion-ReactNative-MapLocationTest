109ef0fdaba3878f16ec48405d9d8e1a2bc7ffd3
Patrick McD <paddyfro@gmail.com>
2019-12-13 14:28:21 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+    ready:false,
+    where: {lat:null, lng:null},
+    error:null
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+  componentDidMount(){
+    let geoOptions = {
+      enableHighAccuracy:true,
+      timeOut: 20000,
+      maximumAge: 60*60*24
+    };
+    this.setState({ready:false});
+    navigator.geolocation.getCurrentPosition(this.geoSuccess,
+                                            this.geoFailure,
+                                            geoOptions );
+  }
+  geoSuccess=(position)=>{
+
+  }
+  geoFailure=(err)=>{
+
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+  render() {
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{text}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
